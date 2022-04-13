f935ef7f52e08a5d6f600e6d7c82aed56838daa2
Yuuuk94 <yuuuk94@gmail.com>
2022-04-13 23:39:26 +0900

    20220413
    헤더 메인 로그인

--- js/incom.js
+++ js/incom.js
@@ -1,51 +1,48 @@
-// header, footer 불러오기
 const elBody = document.querySelector('body');
-const elHeader = document.createElement('header');
-const elFooter = document.createElement('footer');
 
-elBody.prepend(elHeader);
-elBody.append(elFooter);
+// header, footer 불러오기
+fetch("./incom.html")
+.then(function(response) {
+    return response.text();
+})
+.then(function(data) {
+    let parser = new DOMParser();
+	let doc = parser.parseFromString(data, 'text/html');
+    // header 넣기
+    elBody.prepend(doc.body.children[0]);
+    // footer 넣기
+    elBody.append(doc.body.children[0]);
+    // console.log(doc.body.children)
 
-// header events
-let afterHead = function(){
     const elBurger = document.querySelector('.bugermenu');
     const elBp = document.querySelector('.bugermenu p');
     const elMy = document.querySelector('.my');
     const elSearch = document.querySelector('.search');
-    // burgermenu
+    const elNav = document.querySelector('.bugermenu nav');
+    // 버거메뉴
     elBurger.onclick = function(){
         elBp.classList.toggle('active');
         elMy.classList.remove('active');
         elSearch.classList.remove('active');
-
+        if(elBp.classList.contains('active')){
+            elNav.classList.add('active');
+        }else{
+            elNav.classList.remove('active');
+        }
     }
-    // my
+    // 멤버쉽
     elMy.onclick = function(){
         elMy.classList.toggle('active');
         elBp.classList.remove('active');
         elSearch.classList.remove('active');
     }
-    // search
+    // 검색
     elSearch.onclick = function(){
         elSearch.classList.toggle('active');
         elMy.classList.remove('active');
         elBp.classList.remove('active');
     }
-}
-
-$('header').load('incom.html header >div', afterHead);
-$('footer').load('incom.html footer >div');
-
-//fetch 이용하기
-
-// fetch("../incom.html")
-// .then(function(response) {
-//     return response.text();
-// })
-// .then(function(string) {
-//     // elem.innerHTML = string;
-//     console.log(string.value)
-// })
-// .catch((error) => console.log("error:", error));
+})
+.catch((error) => console.log("error:", error));
 
 
