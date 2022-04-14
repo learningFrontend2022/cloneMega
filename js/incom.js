f874995273ec224445b46108b56b7dd939edd38a
Yuuuk94 <yuuuk94@gmail.com>
2022-04-14 18:27:45 +0900

    20220414
    헤더 로그인 팝업, 스토어 로그인 충돌 정리

--- js/incom.js
+++ js/incom.js
@@ -9,7 +9,7 @@ fetch("./incom.html")
     let parser = new DOMParser();
 	let doc = parser.parseFromString(data, 'text/html');
     // header 넣기
-    elBody.prepend(doc.body.children[0]);
+    elBody.prepend(doc.body.children[0], doc.body.children[2]);
     // footer 넣기
     elBody.append(doc.body.children[0]);
     // console.log(doc.body.children)
@@ -19,6 +19,10 @@ fetch("./incom.html")
     const elMy = document.querySelector('.my');
     const elSearch = document.querySelector('.search');
     const elNav = document.querySelector('.bugermenu nav');
+    const elLogin = document.querySelector('.m-login');
+    const elLog = document.querySelector('.login');
+    const elClose = document.querySelector('.log-close');
+
     // 버거메뉴
     elBurger.onclick = function(){
         elBp.classList.toggle('active');
@@ -42,7 +46,21 @@ fetch("./incom.html")
         elMy.classList.remove('active');
         elBp.classList.remove('active');
     }
+    // 로그인
+    elLogin.onclick = function(e){
+        e.preventDefault();
+        // 로그인창 불러오기
+        elLog.classList.add('active');
+        window.onscroll = function(){
+            window.scrollTo(0, 0); 
+        };
+    }     
+    // 로그인창 끄기
+    elClose.onclick = function(){
+        window.onscroll=function(){};
+        elLog.classList.remove('active');
+    }
+
+    
 })
 .catch((error) => console.log("error:", error));
-
-
